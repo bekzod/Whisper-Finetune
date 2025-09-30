11aec2e77a76c0209d1745bde848de6d5d172ab4
bek <bekzod@me.com>
2025-09-30 19:46:03 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,19 +9,24 @@ accelerate launch --multi_gpu --num_processes=2 finetune_whisper_lora.py \
   --train_data ../datasets/train.json \
   --test_data ../datasets/test.json \
   --base_model ../models/whisper-large-v3 \
-  --output_dir ./output/whisper-large-v3-uzbek-2gpu-adalora \
+  --output_dir ./output/whisper-large-v3-uzbek-2xH100-adalora \
   --num_train_epochs 6 \
-  --per_device_train_batch_size 8 \
-  --per_device_eval_batch_size 8 \
-  --gradient_accumulation_steps 8 \
-  --learning_rate 0.0005 \
+  --per_device_train_batch_size 12 \
+  --per_device_eval_batch_size 16 \
+  --gradient_accumulation_steps 6 \
+  --learning_rate 3e-4 \
+  --warmup_ratio 0.1 \
+  --lr_scheduler_type cosine \
   --use_adalora True \
   --lora_r 32 \
   --lora_alpha 64 \
   --lora_dropout 0.05 \
-  --num_workers 8 \
-  --eval_steps 500 \
-  --save_steps 1000 \
+  --num_workers 16 \
+  --group_by_length True \
+  --eval_steps 3000 \
+  --save_steps 6000 \
+  --save_total_limit 5 \
+  --use_compile True \
   --wandb_project whisper-uzbek \
-  --wandb_run_name whisper-v3-uzbek-2gpu-adalora-aggressive \
-  --wandb_tags uzbek,whisper,adalora,2gpu
+  --wandb_run_name whisper-v3-uzbek-2xH100-adalora \
+  --wandb_tags uzbek,whisper,adalora,H100
