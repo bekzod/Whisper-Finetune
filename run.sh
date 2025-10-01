1f3b1c09ee51032c024eccdaac7d99fb60e5f0f7
bek <bekzod@me.com>
2025-10-01 08:54:03 +0500

    fixes

--- run.sh
+++ run.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+
 export NVIDIA_TF32_OVERRIDE=0
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export CUDA_DEVICE_MAX_CONNECTIONS=1
 export CUDA_VISIBLE_DEVICES=0,1
 export WANDB_API_KEY='2dfc22d8af7805df156e7f31ea3bc090ec99d52e'
 
-accelerate launch --multi_gpu --num_processes=2 --config_file ./accelerate_config.yaml finetune.py \
+accelerate launch --multi_gpu --num_processes=2 --config_file ./configs/accelerate.yaml finetune.py \
   --base_model ../models/whisper-large-v3 \
   --output_dir ./output/whisper-large-v3-uzbek-2xH100-adalora \
   --num_train_epochs 6 \
-  --per_device_train_batch_size 16 \
-  --per_device_eval_batch_size 16 \
-  --gradient_accumulation_steps 6 \
-  --learning_rate 3e-4 \
+  --per_device_train_batch_size 8 \
+  --per_device_eval_batch_size 8 \
+  --gradient_accumulation_steps 4 \
+  --learning_rate 2e-4 \
   --warmup_ratio 0.1 \
   --lr_scheduler_type cosine \
   --use_adalora True \
   --lora_r 32 \
   --lora_alpha 64 \
   --lora_dropout 0.05 \
-  --eval_steps 3000 \
-  --save_steps 6000 \
+  --eval_steps 1500 \
+  --save_steps 3000 \
   --save_total_limit 5 \
   --wandb_project whisper-uzbek \
   --wandb_run_name whisper-v3-uzbek-2xH100-adalora \
