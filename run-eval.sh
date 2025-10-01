8f6431e59b7cab728c9df201a00da0bba00ef015
bek <bekzod@me.com>
2025-10-01 13:01:38 +0500

    Update run-eval.sh

--- run-eval.sh
+++ run-eval.sh
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 python3.11 ./evaluation.py \
   --test_data ../datasets/uzbek_voice/data/test/metadata.csv \
-  --model_path ./output/output/whisper-large-v3-uzbek-2xH100-adalora/whisper-large-v3/checkpoint-final-merged \
+  --model_path ./output/whisper-large-v3-uzbek-2xH100-adalora/whisper-large-v3/checkpoint-final-merged \
   --batch_size 16 \
   --num_workers 8 \
   --language Uzbek \
