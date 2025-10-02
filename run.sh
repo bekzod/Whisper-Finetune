6538905d65f05359f3e536bedd441be30ca8794d
bek <bekzod@me.com>
2025-10-02 16:38:37 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ accelerate launch --multi_gpu --num_processes=2 --config_file ./configs/accelera
   --per_device_train_batch_size 96 \
   --per_device_eval_batch_size 192 \
   --gradient_accumulation_steps 1 \
-  --learning_rate 5e-4 \
+  --learning_rate 4e-4 \
   --warmup_ratio 0.15 \
   --lr_scheduler_type cosine \
   --use_adalora True \
