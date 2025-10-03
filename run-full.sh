9c96902e2ec91959cb9bf08580aac3c9531db11b
bek <bekzod@me.com>
2025-10-03 21:44:49 +0500

    Update run-full.sh

--- run-full.sh
+++ run-full.sh
@@ -20,8 +20,8 @@ accelerate launch --multi_gpu --num_processes=2 --config_file ./configs/accelera
   --num_train_epochs 8 \
   --per_device_train_batch_size 32 \
   --per_device_eval_batch_size 64 \
-  --gradient_accumulation_steps 3 \
-  --learning_rate 1e-3 \
+  --gradient_accumulation_steps 2 \
+  --learning_rate 1e-4 \
   --warmup_ratio 0.1 \
   --lr_scheduler_type cosine \
   --use_lora False \
