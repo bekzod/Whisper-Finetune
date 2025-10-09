0456e6328d595e49b9953cee518cf36efb0e489b
bek <bekzod@me.com>
2025-10-09 11:31:26 +0500

    Update run-full.sh

--- run-full.sh
+++ run-full.sh
@@ -31,8 +31,8 @@ accelerate launch --multi_gpu --config_file ./configs/accelerate-single.yaml fin
   --base_model ../models/whisper-large-v3 \
   --output_dir ../models/output-full-finetune \
   --num_train_epochs 5 \
-  --per_device_train_batch_size 80 \
-  --per_device_eval_batch_size 160 \
+  --per_device_train_batch_size 90 \
+  --per_device_eval_batch_size 180 \
   --gradient_accumulation_steps 1 \
   --freeze_encoder_epochs 1 \
   --unfreeze_finish_ratio 0.3 \
