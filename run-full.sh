e6084e8d9bf5a8f48f13e0a702b776d2ba4f85ec
bek <bekzod@me.com>
2025-10-27 00:52:55 +0500

    Update run-full.sh

--- run-full.sh
+++ run-full.sh
@@ -30,7 +30,7 @@ export NCCL_IB_DISABLE=1             # single-node hygiene
 accelerate launch --multi-gpu --config_file ./configs/accelerate.yaml finetune.py \
   --base_model ../models/output-full-finetune/whisper-large-v3-20251026-1138/checkpoint-15000 \
   --output_dir ../models/output-full-finetune \
-  --num_train_epochs 8 \
+  --num_train_epochs 7 \
   --per_device_train_batch_size 14 \
   --per_device_eval_batch_size 32 \
   --gradient_accumulation_steps 2 \
