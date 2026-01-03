e14863fd57f4cdfb124799ff625ea852c9ee31b4
bek <bekzod@me.com>
2026-01-03 19:23:25 +0500

    Update tokenize.sh

--- nemo/tokenize.sh
+++ nemo/tokenize.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 python prepare_tokenizer.py \
   --data_file "../../uzbek.cleaned.txt" \
   --manifest "output/train_manifest.jsonl,output/eval_manifest.jsonl" \
+  --data_root "." \
   --vocab_size 2048 \
   --tokenizer "spe" \
   --spe_type "bpe" \
