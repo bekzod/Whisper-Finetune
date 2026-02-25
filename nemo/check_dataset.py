dff81a76664fd7059ebda69716f9f044c48632b6
bek <bekzod@me.com>
2026-02-25 11:11:38 +0500

    Delete check_dataset.py

--- nemo/check_dataset.py
+++
@@ -1,9 +0,0 @@
-from datasets import load_dataset
-
-ds = load_dataset(
-    "bekzod123/uzbek_voice_3",
-    data_files={"train": "train/metadata.csv"},  # adjust path
-    split="train",
-)
-print(ds)
-print(ds.column_names)
